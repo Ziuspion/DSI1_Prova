d51b09a5205acfc6bd65ab45a1f7a52d102c8c38
Orlando Brito <orlandojuniorbritodasilva@gmail.com>
2026-06-10 10:53:51 -0300

    Add files via upload

--- prova.py
+++ prova.py
@@ -1,5 +1,7 @@
-kg2 = 2
-kg10 = 10
+p2 = 10
+p5 = 20
+p10 = 30
+ac = 20 / 100
 pacote1 = 5
 pacote2 = 6
 pacote3 = 7
@@ -10,4 +12,132 @@ pacote7i = 6
 pacote8 = 2
 pacote9 = 1
 pacote10i = 8
-if kg2 <= 2:
+kg2 = 2
+tkg = pacote1 + pacote2 + pacote3 + pacote4i + pacote5i + pacote6i + pacote7i + pacote8 + pacote9 + pacote10i
+print ("Total de pacotes: 10")
+print ("peso total:", tkg )
+if (pacote1 <= 2):
+    print("pacote1 é menor que 2kg")
+else:
+    print("pacote1 é maior de 2kg")
+if (pacote2 <= 2):
+    print("pacote2 é menor que 2kg")
+else:
+    print("pacote2 é maior de 2kg")
+if (pacote3 <= 2):
+    print("pacote3 é menor que 2kg")
+else:
+    print("pacote3 é maior de 2kg")
+if (pacote4i <= 2):
+    print("pacote4 é menor que 2kg")
+else:
+    print("pacote4 é maior de 2kg")
+if (pacote5i <= 2):
+    print("pacote5 é menor que 2kg")
+else:
+    print("pacote5 é maior de 2kg")
+if (pacote6i <= 2):
+    print("pacote6 é menor que 2kg")
+else:
+    print("pacote6 é maior de 2kg")
+if (pacote7i <= 2):
+    print("pacote7 é menor que 2kg")
+else:
+    print("pacote7 é maior de 2kg")
+if (pacote8 <= 2):
+    print("pacote8 é menor que 2kg")
+else:
+    print("pacote8 é maior de 2kg")
+if (pacote9 <= 2):
+    print("pacote9 é menor que 2kg")
+else:
+    print("pacote9 é maior de 2kg")
+if (pacote10i <= 2):
+    print("pacote10 é menor que 2kg")
+else:
+    print("pacote10 é maior de 2kg")
+if (pacote1 <= 10 > 2):
+    print("pacote1 está entre 2 a 10kg")
+else:
+    print("pacote1 não está entre 2 a 10kg")
+if (pacote2 <= 10 > 2):
+    print("pacote2 está entre 2 a 10kg")
+else:
+    print("pacote2 não está entre 2 a 10kg")
+if (pacote3 <= 10 > 2):
+    print("pacote3 está entre 2 a 10kg")
+else:
+    print("pacote3 não está entre 2 a 10kg")
+if (pacote4i <= 10 > 2):
+    print("pacote4 não está entre 2 a 10kg")
+else:
+    print("pacote4 está entre 2 a 10kg")
+if (pacote5i <= 10 > 2):
+    print("pacote5 está entre 2 a 10kg")
+else:
+    print("pacote5 não está entre 2 a 10kg")
+if (pacote6i <= 10 > 2):
+    print("pacote6 está entre 2 a 10kg")
+else:
+    print("pacote6 não está entre 2 a 10kg")
+if (pacote7i <= 10 > 2):
+    print("pacote7 está entre 2 a 10kg")
+else:
+    print("pacote7 não está entre 2 a 10kg")
+if (pacote8 <= 10 > 2):
+    print("pacote8 não está entre 2 a 10kg")
+else:
+    print("pacote8 está entre 2 a 10kg")
+if (pacote9 <= 10 > 2):
+    print("pacote9 não está entre 2 a 10kg")
+else:
+    print("pacote9 está entre 2 a 10kg")
+if (pacote10i <= 10 > 2):
+    print("pacote10 está entre 2 a 10kg")
+else:
+    print("pacote10 não está entre 2 a 10kg")
+if (pacote1 > 10):
+    print("pacote1 é maior que 10kg")
+else:
+    print("pacote1 não é maior que 10kg")
+if (pacote2> 10):
+    print("pacote2 é maior que 10kg")
+else:
+    print("pacote2 não é maior que 10kg")
+if (pacote3 > 10):
+    print("pacote3 não é maior que 10kg")
+else:
+    print("pacote3 é maior que 10kg")
+if (pacote4i > 10):
+    print("pacote4 não é maior que 10kg")
+else:
+    print("pacote4 é maior que 10kg")
+if (pacote5i > 10):
+    print("pacote5 não é maior que 10kg")
+else:
+    print("pacote5 é maior que 10kg")
+if (pacote6i > 10):
+    print("pacote6 não é maior que 10kg")
+else:
+    print("pacote6 é maior que 10kg")
+if (pacote7i > 10):
+    print("pacote7 não é maior que 10kg")
+else:
+    print("pacote7 é maior que 10kg")
+if (pacote8 > 10):
+    print("pacote8 não é maior que 10kg")
+else:
+    print("pacote8 é maior que 10kg")
+if (pacote9 > 10):
+    print("pacote9 não é maior que 10kg")
+else:
+    print("pacote9 é maior que 10kg")
+if (pacote10i > 10):
+    print("pacote10 não é maior que 10kg")
+else:
+    print("pacote10 é maior que 10kg")
+
+
+
+print("Taxa fixa de R$10 para os pacotes 8 e 9")
+print
